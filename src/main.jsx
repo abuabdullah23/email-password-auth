926d801c58bd6d4600f83bac6903080882ace717
abuabdullah23 <abuobaidazayed@gmail.com>
2023-05-15 16:17:51 +0600

    Route path, element, Outlet,  Header added

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,38 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main.jsx';
+import Home from './Home/Home.jsx';
+import Login from './Login/Login.jsx';
+import Register from './Register/Register.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
+    ]
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
