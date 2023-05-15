1bb7183878716646fb22f06b730cc44d98a719d1
abuabdullah23 <abuobaidazayed@gmail.com>
2023-05-15 22:22:04 +0600

    get user mail and password from reg form

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    // const [email, setEmail] = useState('');
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email, password);
+    }
+
+    // const handleEmailChange = (event) => {
+    //     console.log(event.target.value);
+    //     setEmail(event.target.value)
+    // }
+
+    // const handlePasswordBlur = (event) => {
+    //     console.log(event.target.value);
+    // }
     return (
         <div>
             <h2 className='text-center text-3xl font-bold'>Please Register</h2>
             <div className='mt-5 grid justify-items-stretch'>
                 <div className='justify-self-center'>
-                    <form>
-                        <input type="email" name="email" id="email" placeholder='your email' className='px-5 py-3 bg-slate-200 rounded-md mb-3'/>
+                    <form onSubmit={handleRegister}>
+                        <input type="email" name="email" id="email" placeholder='your email' className='px-5 py-3 bg-slate-200 rounded-md mb-3' />
                         <br />
-                        <input type="password" name="password" id="password" placeholder='your password' className='px-5 py-3 bg-slate-200 rounded-md mb-3'/>
+                        <input type="password" name="password" id="password" placeholder='your password' className='px-5 py-3 bg-slate-200 rounded-md mb-3' />
                         <br />
-                        <button className='border border-slate-600 rounded-md bg-slate-200 hover:bg-slate-950 hover:text-white px-7 py-3 '>Register</button>
+                        <input type="submit" value="Register" className='border border-slate-600 rounded-md bg-slate-200 hover:bg-slate-950 hover:text-white px-7 py-3' />
                     </form>
                 </div>
             </div>
---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuNvK2qN-StKHzjTcdtIStJ6BEHrT6mg8",
+  authDomain: "email-password-auth-f7fa9.firebaseapp.com",
+  projectId: "email-password-auth-f7fa9",
+  storageBucket: "email-password-auth-f7fa9.appspot.com",
+  messagingSenderId: "289321374605",
+  appId: "1:289321374605:web:c66c8501ef4662b00da3c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
