3fc5eff31042ea491004cdaa2d763b2b43867b6f
abuabdullah23 <abuobaidazayed@gmail.com>
2023-05-16 15:23:21 +0600

    Error and success message

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import app from '../firebase/firebase-config';
 
 const auth = getAuth(app);
 
 const Register = () => {
     // const [email, setEmail] = useState('');
+    const [regError, setRegError] = useState('');
+    const [success, setSuccess] = useState('');
+
 
     const handleRegister = (event) => {
         // 1. prevent data
@@ -17,13 +20,17 @@ const Register = () => {
 
         // 3. create user in firebase
         createUserWithEmailAndPassword(auth, email, password)
-        .then(result=>{
-            const loggedUser = result.user;
-            console.log(loggedUser);
-        })
-        .catch(error=>{
-            console.log(error);
-        })
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                setRegError('');
+                event.target.reset('');
+                setSuccess('Account has been created Successfully!')
+            })
+            .catch(error => {
+                console.log(error.message);
+                setRegError(error.message)
+            })
     }
 
     // const handleEmailChange = (event) => {
@@ -43,9 +50,11 @@ const Register = () => {
                         <input type="email" name="email" id="email" placeholder='your email' required className='px-5 py-3 bg-slate-200 rounded-md mb-3' />
                         <br />
                         <input type="password" name="password" id="password" placeholder='your password' required className='px-5 py-3 bg-slate-200 rounded-md mb-3' />
+                        <p className='text-red-600'>{regError}</p>
                         <br />
                         <input type="submit" value="Register" className='border border-slate-600 rounded-md bg-slate-200 hover:bg-slate-950 hover:text-white px-7 py-3' />
                     </form>
+                    <p>{success}</p>
                 </div>
             </div>
         </div>
