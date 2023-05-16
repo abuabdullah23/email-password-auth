ca0fbdd51ac0d4f2b78a777de26a03a466f5ad1c
abuabdullah23 <abuobaidazayed@gmail.com>
2023-05-16 17:21:06 +0600

    Setup for strong password

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -6,30 +6,43 @@ const auth = getAuth(app);
 
 const Register = () => {
     // const [email, setEmail] = useState('');
-    const [regError, setRegError] = useState('');
+    const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
 
 
     const handleRegister = (event) => {
         // 1. prevent data
         event.preventDefault();
+        setSuccess('');
+        setError('');
         // 2. collect form data
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
+
+        // set validate/strong password
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('Please at least one uppercase')
+            return;
+        } else if (!/(?=.*[0-9].*[0-9])/.test(password)) {
+            setError('Please add at least two numbers');
+            return;
+        } else if (password.length < 6) {
+            setError('Please add at least 6 characters in your password')
+        }
 
         // 3. create user in firebase
         createUserWithEmailAndPassword(auth, email, password)
             .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
-                setRegError('');
+                setError('');
                 event.target.reset('');
                 setSuccess('Account has been created Successfully!')
             })
             .catch(error => {
                 console.log(error.message);
-                setRegError(error.message)
+                setError(error.message)
             })
     }
 
@@ -50,7 +63,7 @@ const Register = () => {
                         <input type="email" name="email" id="email" placeholder='your email' required className='px-5 py-3 bg-slate-200 rounded-md mb-3' />
                         <br />
                         <input type="password" name="password" id="password" placeholder='your password' required className='px-5 py-3 bg-slate-200 rounded-md mb-3' />
-                        <p className='text-red-600'>{regError}</p>
+                        <p className='text-red-600'>{error}</p>
                         <br />
                         <input type="submit" value="Register" className='border border-slate-600 rounded-md bg-slate-200 hover:bg-slate-950 hover:text-white px-7 py-3' />
                     </form>
---
+++ src/links.js
@@ -0,0 +1,4 @@
+/**
+ * password validate regex: https://stackoverflow.com/questions/5142103/regex-to-validate-password-strength
+ * 
+*/
