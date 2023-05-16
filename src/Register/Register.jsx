01ce18b5329e9f1c5d0d18d2706a6e84e676ece9
abuabdullah23 <abuobaidazayed@gmail.com>
2023-05-16 10:08:18 +0600

    Registration method added, input box require

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+import app from '../firebase/firebase-config';
+
+const auth = getAuth(app);
 
 const Register = () => {
     // const [email, setEmail] = useState('');
 
     const handleRegister = (event) => {
+        // 1. prevent data
         event.preventDefault();
+        // 2. collect form data
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
+
+        // 3. create user in firebase
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(result=>{
+            const loggedUser = result.user;
+            console.log(loggedUser);
+        })
+        .catch(error=>{
+            console.log(error);
+        })
     }
 
     // const handleEmailChange = (event) => {
@@ -24,9 +40,9 @@ const Register = () => {
             <div className='mt-5 grid justify-items-stretch'>
                 <div className='justify-self-center'>
                     <form onSubmit={handleRegister}>
-                        <input type="email" name="email" id="email" placeholder='your email' className='px-5 py-3 bg-slate-200 rounded-md mb-3' />
+                        <input type="email" name="email" id="email" placeholder='your email' required className='px-5 py-3 bg-slate-200 rounded-md mb-3' />
                         <br />
-                        <input type="password" name="password" id="password" placeholder='your password' className='px-5 py-3 bg-slate-200 rounded-md mb-3' />
+                        <input type="password" name="password" id="password" placeholder='your password' required className='px-5 py-3 bg-slate-200 rounded-md mb-3' />
                         <br />
                         <input type="submit" value="Register" className='border border-slate-600 rounded-md bg-slate-200 hover:bg-slate-950 hover:text-white px-7 py-3' />
                     </form>
